7e6726f1b76979b7ee0d027c33dea68113585f9e
CHRISTOPHER VANDERGRIFF <rl239129@rlas-macair-38.local>
2018-11-28 10:24:04 -0600

    Initial Commit

---
+++ threeLetterWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  threeLetterWord
+//
+//  Created by CHRISTOPHER VANDERGRIFF on 11/28/18.
+//  Copyright © 2018 CHRISTOPHER VANDERGRIFF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
